79ee89619e66b43cc8d47d10a18d2ecaf1f50489
Laszlo <44098779+LaszloWa@users.noreply.github.com>
2020-03-10 21:02:56 +0100

    created components to display homepage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
+
 import './App.css';
+
+import HomePage from './pages/homepage';
 
 function App() {
   return (
     <div>
-      
+      <HomePage />
     </div>
   );
 }

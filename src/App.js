14cd57c4c5561e6cf7f0aeb7f8c14eeb759536f3
Laszlo <44098779+LaszloWa@users.noreply.github.com>
2020-03-12 17:52:56 +0100

    added sign-in and sign-up page, sign-in component and button component

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage';
 import ShopPage from './pages/shop/shop.component'
+import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component';
+
 
 function App() {
   return (
@@ -14,9 +16,10 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route exact path="/shop" component={ShopPage} />
+        <Route exact path="/signin" component={SignInAndSignUp} />
       </Switch>
     </div>
-  );
+  )
 }
 
 export default App;

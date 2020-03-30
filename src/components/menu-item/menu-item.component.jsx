1844a3f56794abcb83ca912618926768e8fe8c83
Laszlo <44098779+LaszloWa@users.noreply.github.com>
2020-03-30 20:23:11 +0200

    implemented collection page to display each collection individually, changed shop data from an array to an object to make querying easier, set up routes from the homepage to the different collection pages

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -4,8 +4,11 @@ import { withRouter } from 'react-router-dom'
 import './menu-item.styles.scss';
 
 
-const MenuItem = ({ title, imageUrl, size }) => ( 
-    <div className={`${size} menu-item`}>
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => ( 
+    <div 
+        className={`${size} menu-item`}
+        onClick={() => history.push(`${match.url}${linkUrl}`)}
+    >
         <div 
             style={{backgroundImage: `url(${imageUrl})`}}className="background-image"
         />

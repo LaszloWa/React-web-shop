228065507377ff01f31f98425d842495bb9c760b
Laszlo <44098779+LaszloWa@users.noreply.github.com>
2020-04-06 17:22:54 +0200

    moved shop.data into Firestore and changed CollectionsPreview to handle starting with an empty collection as data is fetched first. Also implemented a higher order component containing a loading animation and implemented it in the Shop component

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -36,8 +36,28 @@ const config = {
     }
 
     return userRef;
-  }
+  };
 
+  export const convertCollectionsSnapshotToMap = (collections) => {
+    const transformedCollection = collections.docs.map(
+      doc => {
+        const { title, items } = doc.data();
+
+        return {
+          routeName: encodeURI(title.toLowerCase()),
+          id: doc.id,
+          title,
+          items,
+        }
+      }
+    )
+    
+    return transformedCollection.reduce((accumulator, collection) => {
+      accumulator[collection.title.toLowerCase()] = collection;
+      return accumulator;
+    }, {});
+  }
+  
   firebase.initializeApp(config);
 
   export const auth = firebase.auth();

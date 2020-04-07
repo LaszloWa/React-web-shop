6ee56aa716eeedb6398eb94e5ebcc76849d49303
Laszlo <44098779+LaszloWa@users.noreply.github.com>
2020-04-07 19:52:40 +0200

    moved API call fetching shop data from Firebase to Redux

--- src/redux/shop/shop.reducer.js
+++ src/redux/shop/shop.reducer.js
@@ -2,15 +2,29 @@ import ShopActionTypes from './shop.types'
 
 const INITAL_STATE = {
     collections: null,
+    isFetching: false,
+    errorMessage: undefined,
 }
 
 const shopReducer = ( state = INITAL_STATE, action ) => {
     switch (action.type) {
-        case ShopActionTypes.UPDATE_COLLECTIONS:
+        case ShopActionTypes.FETCH_COLLECTIONS_START:
+            return {
+                ...state,
+                isFetching: true,
+            };
+        case ShopActionTypes.FETCH_COLLECTIONS_SUCCESS:
             return {
                 ...state,
                 collections: action.payload,
-            }
+                isFetching: false,
+            };
+        case ShopActionTypes.FETCH_COLLECTIONS_FAILURE:
+            return {
+                ...state,
+                isFetching: false,
+                errorMessage: action.payload,
+            };
         default:
             return state;
     }
